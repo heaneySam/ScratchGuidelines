ae2c12585f12ffa419cb06669e91bdd3aa647278
heaneySam <heaney.sam@gmail.com>
2024-06-08 20:40:43 +0100

    Got rid of some more errors

--- tableapp/filters.py
+++ tableapp/filters.py
@@ -9,34 +9,6 @@ from django.db.models import Count
 
 class TrustGuidelineFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(method='filter_search', label="Search")
-    # medical_speciality = django_filters.MultipleChoiceFilter(
-    #     choices=TrustGuideline.objects.values_list('medical_speciality', 'medical_speciality').distinct(),
-    #     widget=forms.CheckboxSelectMultiple
-    # )
-    search = django_filters.CharFilter(method='filter_search', label="Search", widget=forms.TextInput(attrs={
-        'hx-trigger': 'keyup changed delay:500ms clear:none from:input',
-        'hx-target': '#table-container',  # Make sure this ID matches the container in your HTML
-        'hx-get': reverse_lazy('trust_guideline_view'),
-        'class': 'form-control',
-        'placeholder': 'Search names and descriptions...'
-    }))
-    trust = django_filters.ModelChoiceFilter(queryset=Trust.objects.all(), empty_label=None, widget=forms.Select(attrs={
-        'hx-get': reverse_lazy('trust_guideline_view'),
-        'hx-trigger': 'change',  # This will trigger the filter when the dropdown changes
-        'hx-target': '#table-container',
-        'class': 'form-control'
-    }))
-
-    def __init__(self, *args, **kwargs):
-        super(TrustGuidelineFilter, self).__init__(*args, **kwargs)
-        # Set default trust if not provided by the user
-        if 'trust' not in self.data:
-            # Select a default trust, can be dynamic based on conditions
-            default_trust = Trust.objects.first()  # or any other logic to select a default trust
-            self.form.fields['trust'].initial = default_trust
-            self.queryset = self.queryset.filter(trust=default_trust)
-
-
 
     def filter_search(self, queryset, name, value):
         return queryset.filter(
@@ -45,15 +17,5 @@ class TrustGuidelineFilter(django_filters.FilterSet):
 
     class Meta:
         model = TrustGuideline
-        fields = ['search', 'medical_speciality']
+        fields = ['search']
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        speciality_choices = TrustGuideline.objects.values('medical_speciality').annotate(
-            count=Count('medical_speciality')
-        ).order_by('-count')
-
-        self.filters['medical_speciality'].extra['choices'] = [
-            (s['medical_speciality'], f"{s['medical_speciality']} ({s['count']})")
-            for s in speciality_choices
-        ]
