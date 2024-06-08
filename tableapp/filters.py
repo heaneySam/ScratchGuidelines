37ba8755e4163b65181e4d32eba3a02eba39cb86
heaneySam <heaney.sam@gmail.com>
2024-06-08 19:40:58 +0100

    made some small changes

--- tableapp/filters.py
+++ tableapp/filters.py
@@ -6,11 +6,38 @@ from django.db.models import Count
 
 
 class TrustGuidelineFilter(django_filters.FilterSet):
+<<<<<<< Updated upstream
     search = django_filters.CharFilter(method='filter_search', label="Search")
     medical_speciality = django_filters.MultipleChoiceFilter(
         choices=TrustGuideline.objects.values_list('medical_speciality', 'medical_speciality').distinct(),
         widget=forms.CheckboxSelectMultiple
     )
+=======
+    search = django_filters.CharFilter(method='filter_search', label="Search", widget=forms.TextInput(attrs={
+        'hx-trigger': 'keyup changed delay:500ms clear:none from:input',
+        'hx-target': '#table-container',  # Make sure this ID matches the container in your HTML
+        'hx-get': reverse_lazy('trust_guideline_view'),
+        'class': 'form-control',
+        'placeholder': 'Search names and descriptions...'
+    }))
+    trust = django_filters.ModelChoiceFilter(queryset=Trust.objects.all(), empty_label=None, widget=forms.Select(attrs={
+        'hx-get': reverse_lazy('trust_guideline_view'),
+        'hx-trigger': 'change',  # This will trigger the filter when the dropdown changes
+        'hx-target': '#table-container',
+        'class': 'form-control'
+    }))
+
+    def __init__(self, *args, **kwargs):
+        super(TrustGuidelineFilter, self).__init__(*args, **kwargs)
+        # Set default trust if not provided by the user
+        if 'trust' not in self.data:
+            # Select a default trust, can be dynamic based on conditions
+            default_trust = Trust.objects.first()  # or any other logic to select a default trust
+            self.form.fields['trust'].initial = default_trust
+            self.queryset = self.queryset.filter(trust=default_trust)
+
+
+>>>>>>> Stashed changes
 
     def filter_search(self, queryset, name, value):
         return queryset.filter(
