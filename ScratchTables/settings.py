5daeeed70c833724912b2a3c33e5b5f2be351a99
heaneySam <heaney.sam@gmail.com>
2025-01-09 10:07:06 +0000

    Removed allauth middleware

--- ScratchTables/settings.py
+++ ScratchTables/settings.py
@@ -123,7 +123,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'allauth.account.middleware.AccountMiddleware',  # Add this line
+    # 'allauth.account.middleware.AccountMiddleware',  # Add this line
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
